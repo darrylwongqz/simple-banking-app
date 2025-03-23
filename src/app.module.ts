75ee9569dd1faf09485b18c27d91539d7985b227
darrylwongqz <dwqz92@gmail.com>
2025-03-23 23:43:28 +0800

    added boilerplate to fill in for framework

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BankModule } from './bank/bank.module';
+import { UserModule } from './user/user.module';
+import { TransactionModule } from './transaction/transaction.module';
 
 @Module({
-  imports: [],
+  imports: [BankModule, UserModule, TransactionModule],
   controllers: [AppController],
   providers: [AppService],
 })
